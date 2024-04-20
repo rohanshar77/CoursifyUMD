8234c559b68e7214b17e2d064fab896d6ef4bc43
rohanshar77 <rohan77@icloud.com>
2024-04-20 11:29:43 -0400

    backend done and tested

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 @app.route('/search', methods=['POST'])
-def receive_data():
+async def receive_data():
     # Check if the request contains JSON data
     if request.is_json:
         # Parse the JSON data
@@ -15,14 +15,16 @@ def receive_data():
         major = data['major']
         industry = data['industry']
         interests = data['interests']
-        include_grad = data['include_grad']
 
-        major_courses, career_courses, interest_courses = query_pinecone(major, industry, interests, include_grad)
+        major_courses, career_courses, interest_courses = await query_pinecone(major, industry, interests)
+        major_courses = list(major_courses.values())
+        career_courses = list(career_courses.values())
+        interest_courses = list(interest_courses.values())
 
-        # Return a response to the frontend
         return jsonify(major_courses, career_courses, interest_courses), 200
     else:
         return jsonify({"status": "error", "message": "Request was not JSON"}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
